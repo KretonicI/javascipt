208ccce95bcdb5e2d094ba79b0c03d01879cc063
office10 <office10@ucione.local>
2024-04-10 19:22:05 +0200

    dodaj

---
+++ skripte/script2.js
@@ -0,0 +1,18 @@
+//var firstName = "Marko"
+//var firstName, lastName, price, discount, fullPrice;
+
+ firstName = "John";
+ lastName = "Doe"
+
+ price = 19.90;
+ discount = 0.10;
+
+if (x == 1){
+x = 2;
+console.log("x= " + x)}
+
+if (a) {
+    var z = 44
+    return 0;
+}
+fullPrice = price * 100 / discount;
